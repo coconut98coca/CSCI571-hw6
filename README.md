3ee0e501207204fe8cecf4cd8cc1a2149b27287f
qujq <qujq@outlook.com>
2021-06-02 16:15:43 +0800

    request ticketmaster and get response

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# hw6
+## hw6
+
+# ativate virtual environment on Windows: 
+```Shell
+env/Scripts/activate
+```
+
+# Run code:
+```Shell
+python main.py
+```
+
+# deploy on Google Cloud Platform
+Need to change backend_url (in base.html) from "http://127.0.0.1:8080/search?" to "https://myfirstpython-9901.wl.r.appspot.com/search?"
+```Shell
+gcloud app deploy
+```
+# To browse
+```Shell
+gcloud app browse
+```
+
+# exit the virtual environment
+```Shell
+deactivate
+```
--- main.py
+++ main.py
@@ -14,24 +14,67 @@
 
 # [START gae_python38_app]
 # [START gae_python3_app]
-from flask import Flask, render_template, request, jsonify
+import requests
+import json
+from geolib import geohash
+from flask import Flask, request, jsonify
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
-
+get_geo_url = "https://ipinfo.io/?token=c4eba8a0a82929"
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return render_template('base.html')
+    return app.send_static_file('base.html')
 
 @app.route('/search', methods=["GET"])
 def do_search():
+    ticketmaster_base_url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Qf8PRg3ggae12R8TRPqlTRnJdD6EE3q3"
     arg = request.args
-    print("++++++++", arg)
-    return {}
+    search_data = arg.to_dict()
+    # print("++++++++", search_data)
+
+    # geo point
+    latitude = search_data["latitude"]
+    longitude = search_data["longitude"]
+    geoPoint = geohash.encode(latitude, longitude, 7)
+    ticketmaster_base_url += "&geoPoint=" + geoPoint
+    # radius
+    radius = search_data["distance"]
+    ticketmaster_base_url += "&radius=" + radius
+    # segment id
+    segment_choose = search_data["category"]
+    if segment_choose != "Default":
+        if segment_choose == "Music":
+            segment_id = "KZFzniwnSyZfZ7v7nJ"
+        if segment_choose == "Sports":
+            segment_id = "KZFzniwnSyZfZ7v7nE"
+        if segment_choose == "ArtsTheatre":
+            segment_id = "KZFzniwnSyZfZ7v7na"
+        if segment_choose == "Film":
+            segment_id = "KZFzniwnSyZfZ7v7nn"
+        if segment_choose == "Miscellaneous":
+            segment_id = "KZFzniwnSyZfZ7v7n1"
+        ticketmaster_base_url += "&segmentId=" + segment_id
+    # unit
+    ticketmaster_base_url += "&unit=" + "miles"
+    # keyword
+    keyword = search_data["keyword"]
+    ticketmaster_base_url += "&keyword=" + keyword
+    print(ticketmaster_base_url)
+    # request api
+    ticketmaster_response = requests.get(ticketmaster_base_url).json()
+    print(json.dumps(ticketmaster_response))
+    ticketmaster_response = jsonify(ticketmaster_response)
+    ticketmaster_response.headers['Access-Control-Allow-Origin'] = '*'
+    # if(ticketmaster_response.get("page").get("number") == 0):
+    #     print("No results.")
+    
+    print("----")
+    return ticketmaster_response
 
 
 
