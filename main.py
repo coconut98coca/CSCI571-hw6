b66dfd1e66abcb40b2b5883188580584a14b5de6
qujq <qujq@outlook.com>
2021-06-07 22:10:56 +0800

    fix: lat&long of location; detail block layout; price range display

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python38_app]
-# [START gae_python3_app]
 import requests
 import json
 from geolib import geohash
 from flask import Flask, request, jsonify
 
-
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
 app = Flask(__name__)
 get_geo_url = "https://ipinfo.io/?token=c4eba8a0a82929"
 
@@ -36,11 +17,6 @@ def do_search():
     arg = request.args
     search_data = arg.to_dict()
     print("++++++++", search_data)
-
-    # # convert location into latitude and longitude
-    # location = search_data.get("location")
-    # if location != "":
-    #     pass
 
     # geo point
     latitude = search_data["latitude"]
@@ -75,18 +51,10 @@ def do_search():
     print(json.dumps(ticketmaster_response))
     ticketmaster_response = jsonify(ticketmaster_response)
     ticketmaster_response.headers['Access-Control-Allow-Origin'] = '*'
-    # if(ticketmaster_response.get("page").get("number") == 0):
-    #     print("No results.")
-    
     print("----")
     return ticketmaster_response
 
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
